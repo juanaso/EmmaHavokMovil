2a81a2f2a39d676103295bd6d672c00869e503bd
juan Sulbaran <sulbaranja@gmail.com>
2016-02-16 15:12:54 -0430

    commit3

--- app/src/main/java/com/example/juanaso/emmahavokmovil/MainActivity.java
+++ app/src/main/java/com/example/juanaso/emmahavokmovil/MainActivity.java
@@ -1,31 +1,122 @@
 package com.example.juanaso.emmahavokmovil;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    NavigationView navigationView = null;
+    Toolbar toolbar = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setContentView(R.layout.emma_nav);
+
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //toolbar.setLogo(R.drawable.icon1);
+        toolbar.setTitle(" ");
         setSupportActionBar(toolbar);
 
+        //setFragment-----------------------------------
+        VideoYoutubeList fragment = new VideoYoutubeList();
+        android.support.v4.app.FragmentTransaction fragmentTransaction =
+                getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragmen_container, fragment);
+        fragmentTransaction.commit();
+        //------------------------------------------------
 
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        //----------------------------------------------------------
+
+
+
+
+    }//onCreate
+
+
+
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.wiki) {
+
+
+
+
+        } else if (id == R.id.ask) {
+
+
+
+        } else if (id == R.id.videosvlogs) {
+
+
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        System.out.println("la vie");
     }
 
     @Override
@@ -42,4 +133,22 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
+
+
+    //----------------------------------------------------
+    private void pantalla(String string) {
+        Toast.makeText(getApplicationContext(), string, Toast.LENGTH_SHORT)
+                .show();
+    }
+
+
+
+
+
+
+
+
 }
---
+++ app/src/main/java/com/example/juanaso/emmahavokmovil/VideoYoutubeList.java
@@ -0,0 +1,29 @@
+package com.example.juanaso.emmahavokmovil;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class VideoYoutubeList extends Fragment {
+
+
+    public VideoYoutubeList() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_video_youtube_list, container, false);
+    }
+
+}
